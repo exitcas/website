18ba9c0180b5eb93513f9c561b50ef422a3f72ff
Lucas <83518257+exitcas@users.noreply.github.com>
2022-12-30 21:21:53 -0300

    Update _last.php

--- _last.php
+++ _last.php
@@ -3,7 +3,7 @@ if (!isset($p)) { header("Location: /"); } ?>
 <p><a href="https://pixabay.com/photos/capibara-rodent-pig-water-cabiai-2528359/" id="link" style="font-size:small"><?= $trans["bkg_src"]; ?></a></p>
 </main>
 <footer>
-&copy;2021-22 Luqaska
+&copy;2021-23 Lucas
 <button id="setBkg" onclick="setBkg()" style="float:right"><i class="fa fa-repeat"></i> <span id="sBtxt">Loading</span></button>
 </footer>
 <script src="/script.js"></script>
